0e02892e2c752e4a2ee5697e30489cc9ecfe4e92
FHachez <floran.hachez@student.uclouvain.be>
2014-07-25 16:39:44 +0200

    Add: CDR toString tests

--- src/test/scala/Model/CDRSpec.scala
+++ src/test/scala/Model/CDRSpec.scala
@@ -14,9 +14,37 @@ class CDRSpec extends FlatSpec {
 			SMS)
 	}
 
-	"CDR header" should "be in a specific order" in {
-		val f = fixture
-		assert( f.cdr.header(",") == "fromUserId,toUserId,fromOperator,toOperator,duration,date,type" )
+	"CDR.toString fields" should "be in the same order as the fields return by the header function" in {
+		val cdr = fixture.cdr
+		val fields = cdr.toString(",").split(",")
+		val header = cdr.header(",").split(",")
+
+		header.zip(fields).foreach{ case (h,f) =>
+				h match {
+					case "fromUserId" => assert( f == cdr.fromUser.id.toString )
+					case "toUserId" => assert( f == cdr.toUser.id.toString )
+					case "fromOperator" => assert( f == cdr.fromUser.operator.name )
+					case "toOperator" => assert( f == cdr.toUser.operator.name )
+					case "duration" => assert( f == cdr.duration.toString("%y%m%d") )
+					case "date" => assert( f == cdr.date.toString("%y%m%d") )
+					case "type" => assert( f == CDRType.toString(cdr.cdrType))
+					case _ => assert(false)
+				}
+			}
+	}
+	it should "remain the same" in {
+		val cdr = fixture.cdr
+		val headerFields = cdr.header(",").split(",")
+		val fields = Array(
+			"fromUserId", 
+			"toUserId",  
+			"fromOperator", 
+			"toOperator", 
+			"duration", 
+			"date", 
+			"type"
+			)
+		fields.foreach( f => assert(headerFields.contains(f)) )
 	}
 
 }
