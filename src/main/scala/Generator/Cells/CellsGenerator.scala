10dbdf893e8e7e940d4634700a90f301a66c1287
FHachez <floran.hachez@student.uclouvain.be>
2014-07-30 14:31:55 +0200

    Add: Cell, drop method

--- src/main/scala/Generator/Cells/CellsGenerator.scala
+++ src/main/scala/Generator/Cells/CellsGenerator.scala
@@ -13,6 +13,11 @@ abstract class CellsGenerator(){
 		* @return an array of cells
 		*/
 	def generate(operators: Array[Operator]) : Array[Cell]
+
+	/** Compute if the cell willl drop the next call or not
+	 * @return  true if the cell drop the call false otherwise
+	 */
+	def drop : Boolean 
 }
 
 /** Generate cell located at random position (x, y)
@@ -23,13 +28,15 @@ abstract class CellsGenerator(){
 	* @param latMax Double, the latitude of the top right corner of the square, default = 1.0
 	* @param lonMin Double, the longitude of the bottom left corner of the square, default = 0.0
 	* @param lonMax Double, the longitude of the top right corner of the square, default = 1.0
+	* @param dropProbability The probability that the cell have to drop a call
 	*/
 class BasicCellsGenerator(
 	val nCells: Int,
 	val latMin: Double = 0,
 	val latMax: Double = 1,
 	val lonMin: Double = 0,
-	val lonMax: Double = 1
+	val lonMax: Double = 1,
+	val dropProbability: Double = 0.01
 ) extends CellsGenerator {
 	private val rand = new Random( (new DateTime()).getMillisOfDay() )
 
@@ -42,6 +49,9 @@ class BasicCellsGenerator(
 		latMin + (rand.nextDouble() * (latMax - latMin)),
 		lonMin + (rand.nextDouble() * (lonMax - lonMin))
 	)
+
+	override def drop = rand.nextDouble <= dropProbability
+
 }
 
 
--- src/main/scala/Model/Cell.scala
+++ src/main/scala/Model/Cell.scala
@@ -1,13 +1,29 @@
 package model
 
+import scala.util.Random
+import scala.util.Random._
+import org.joda.time.DateTime
+import org.joda.time.DateTime._
+
 case class Location(lat: Double, lon: Double)
 
+/**
+ * @param dropProbability The probability that the cell have to drop a call
+ */
 class Cell (
 	val id: Long,
 	val location: Location,
 	val operator: Operator,
-	val cellsNeighbor: Array[Cell]
-)extends Serializable
+	val cellsNeighbor: Array[Cell],
+	val dropProbability: Double = 0.01
+)extends Serializable {
+	private val rand = new Random( (new DateTime()).getMillisOfDay() )
+
+	/** Compute if the cell willl drop the next call or not
+	 * @return  true if the cell drop the call false otherwise
+	 */
+	def drop : Boolean = rand.nextDouble <= dropProbability 
+}
 
 
 /** Default Cell for testing
